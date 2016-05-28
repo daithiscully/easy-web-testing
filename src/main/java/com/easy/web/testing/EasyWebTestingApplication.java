80b42f580a1ca7fcb7ca064872960cfff312953e
daithiscully <scully129@live.com>
2016-05-28 22:31:49 +0100

    Initial commit

---
+++ src/main/java/com/easy/web/testing/EasyWebTestingApplication.java
@@ -0,0 +1,12 @@
+package com.easy.web.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWebTestingApplication.class, args);
+	}
+}
